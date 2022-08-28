36af1746ae0d9e854777513a43147d2ff378a311
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-29 01:05:35 +0530

    Create Lect_01.cpp

---
+++ Lect_01.cpp
@@ -0,0 +1,27 @@
+                                              //>>>> challanges in software development<<<<\\
+
+
+// Module Objectives
+//>> Understand why software is still developed and not constructed.
+//>> Understand why software projects fail.
+//>> Take a glimpse of the remedial measures/
+//>> KYC: Know your course
+
+// Module outline
+//>> Is software Engineering ?
+//>>>> Evolution of Engineering Domains.
+//>> Software: Development ---> Construction
+//>> KYC: Know Your Course
+//>>>> Course outline
+//>>>> Course Text Book
+//>>>> Course Examples
+//>>>> Course TAS
+
+// SOFTWARE ENGINEERING
+// The order of the day is to refer to software development as spftware engineering.
+// Can we really Construct Software as civil engineers build bridges?
+
+// Engineering : Skills of construction
+//>> Civil Engineering
+//>>>> Construction od buildings
+//>> Mechinical Engineering
