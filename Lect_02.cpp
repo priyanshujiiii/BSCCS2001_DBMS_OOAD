15456a8551b3ec98e34c138b74bafb314453e5f0
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-09-01 16:39:20 +0530

    Update Lect_02.cpp

--- Lect_02.cpp
+++ Lect_02.cpp
@@ -1,1 +1,26 @@
+//Understand why software is complex
+//Study the elements of Compplexity
+
+
+//>> Defining software complexity
+//>> Four elemnts of complexity
+//>>>> The Complxity of the problem domain.
+//>>>> The difficulty of managing the development process
+//>>>> The Flexibility Possible through software
+//>>>> The Probles of characterizing the behavior Discrete Systems.
+
+
+//>> Personal or limited-use Software
+//>> Industrial-Strengh Software
+
+//>> Limited set of behaviours
+//>> Not very complex
+//>> Specified , constructed , maintained , and used by the same person or a small group.
+//>>>> Amateur programmer , or
+//>>>> Professional developer woking in isolation
+//>> Tend to have short life span 
+//>> Can be thrown away and replaced with entirely new software rather
+//    attempt to reuse them , repair them, or extend their functionality
+//>> Generally more tedious then difficult to develop 
+//>> Techniques to develop them is of little interest here
 
