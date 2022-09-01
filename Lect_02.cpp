6454853a7541dd74fdd8c17167b75170022ddfc2
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-09-01 16:55:15 +0530

    Update Lect_02.cpp

--- Lect_02.cpp
+++ Lect_02.cpp
@@ -24,3 +24,36 @@
 //>> Generally more tedious then difficult to develop 
 //>> Techniques to develop them is of little interest here
 
+//>> Exhbit a very rich set of behaviors - reactive systems that drive or are
+//   driven by events in the physical world.
+//>> Works with scarce resources - time, space, power,.....
+//>> Maintain the intergrity of millions of record while allowing 
+//   concurrent updates and quries - airline booking 
+//>> Commands and contols of real-world entites- routing of air or
+//   railway traffic
+//>> Depended by many users over time on proper functioning 
+//>> usually based onframeworks that simplify the creation of
+     domain-specific applications
+
+// Complexity of industrial strength strength software systems of the 
+// human intellectual capacity
+
+
+// Software is inherently complex
+//>> The Complity of the problem domain.
+//>> The difficulty of managing the development process
+//>> The Flexibility possible through software
+//>> The problems of characterizing the behaviour of Discrete Systems
+
+// The complexity of the problem domain
+//>> Domains are difficult to understand. For exampele:
+//>>>> Electronic system of a multi-engine aircraft
+//>>>> Merchant shipping
+//>>>> Online Trading and reconciliation
+//>> Functional Requirments are 
+//>>>> Complex to master 
+//>>>> Often are competing , even contradictory
+//>> Non- Functional Requirements( usability , perfomanc,
+cost, survivability, and reliabiliy) are 
+//>>>> often implicit
+//>>>> difficult to justify in budget
